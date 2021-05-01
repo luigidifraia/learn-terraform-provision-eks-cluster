e504edfa653afd9baa9fb5cbfa79838b46fdfc23
Luigi Di Fraia <luigi.difraia@nominet.uk>
2021-05-01 16:03:47 +0100

    Pin EKS module version

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,5 +1,7 @@
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
+  version         = "15.1.0"
+
   cluster_name    = local.cluster_name
   cluster_version = "1.19"
   subnets         = module.vpc.private_subnets
